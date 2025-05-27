4e96ea54f80dba7abe332588d2adce91deb4abc0
hoangnam2803 <yasuodarkwind@gmail.com>
2025-05-27 09:08:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -56,9 +56,9 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
+    "please please?",
+    "baby please...",
+    "chọn lại điiiiiiii!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
